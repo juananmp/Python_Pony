a3f47c1bd1d8a2107e042fff3fbace8887ae19f8
juananmp <jantoniocantos@gmail.com>
2018-03-02 11:06:41 +0100

    He añadido una nueva clase coche con sus campos e inserto un c1

--- BBDDMySql.py
+++ BBDDMySql.py
@@ -10,6 +10,11 @@ class Person(db.Entity):
     name = Required(str)
     age = Required(int)
 
+class Coche(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    puertas = Required(int)
+
 #Para mostrar por consola
 sql_debug(True)
 #Crea la tabla sino esta creada
@@ -19,3 +24,6 @@ db.generate_mapping(create_tables=True)
 #uso with aunque podria usar @db_session
 with db_session:
     p1 = Person(name='John', age=20)
+
+with db_session:
+    c1 = Coche(name='Opel', puertas=4)
