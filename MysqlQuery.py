e199fce236aa4a0f1490883a4bea77793674372f
juananmp <jantoniocantos@gmail.com>
2018-03-02 11:06:59 +0100

    He hecho otra query pero para la clase Coche

--- MysqlQuery.py
+++ MysqlQuery.py
@@ -1,6 +1,7 @@
 from pony.orm import *
 import pony.orm as pony
-from BBDDMySql import Person
+from BBDDMySql import Person, Coche
+
 
 @pony.db_session
 def ejecutor_queries():
@@ -9,7 +10,16 @@ def ejecutor_queries():
     #muestra todos los usuarios pero sin redundancias
     #query1 = select(p.name for p in Person).show()
     #Muestra todo el contenido de la tabla persona
+    #Con esta query se cargan varios datos en las tablas
     query1 = select(p for p in Person).show()
     print(query1)
 
-ejecutor_queries()
+#ejecutor_queries()
+
+@pony.db_session
+def ejecutor_queries2():
+    #Con esta query el dato se carga tres datos
+    query2 = pony.select(c for c in Coche).show()
+    print(query2)
+
+ejecutor_queries2()
